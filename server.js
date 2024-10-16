2376a0db52f6c5d409647c9af34ba845d41e30ec
NathalieBaskin <nathalie.baskin@utb.ecutbildning.se>
2024-10-16 19:51:07 +0200

    Fungerande produktkort

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Servera statiska filer från public-mappen
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route för startsidan
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Route för produktdetaljer-sidan
+app.get('/product-details.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'product-details.html'));
+});
+
+// Starta servern
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
